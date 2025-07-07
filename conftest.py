ac0cdd5ad605aca280c3d9c515c8c14ad190b399
ShahabasAman <shezilaman@gmail.com>
2025-07-07 14:42:17 +0530

    fix: CI file workflow

--- conftest.py
+++ conftest.py
@@ -1,16 +1,43 @@
+# import pytest
+# from selenium import webdriver
+# from selenium.webdriver.chrome.service import Service
+
+# @pytest.fixture
+# def browser():
+#     options = webdriver.ChromeOptions()
+#     options.binary_location = "/usr/local/bin/chromium-114/chrome"
+
+#     service = Service(executable_path="/usr/local/bin/chromedriver")
+
+#     driver = webdriver.Chrome(service=service, options=options)
+#     driver.maximize_window()
+
+#     yield driver 
+#     driver.quit()
+
 import pytest
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
+import os
 
 @pytest.fixture
 def browser():
     options = webdriver.ChromeOptions()
-    options.binary_location = "/usr/local/bin/chromium-114/chrome"
 
-    service = Service(executable_path="/usr/local/bin/chromedriver")
+    # For CI: run in headless mode
+    Headless = True
+    if Headless:
+        options.add_argument("--headless=new")
+        options.add_argument("--no-sandbox")
+        options.add_argument("--disable-dev-shm-usage")
 
-    driver = webdriver.Chrome(service=service, options=options)
+    # If running locally with custom Chrome
+    chrome_path = "/usr/local/bin/chromium-114/chrome"
+    if os.path.exists(chrome_path):
+        options.binary_location = chrome_path
+
+    # Use Selenium Manager to auto-download ChromeDriver
+    driver = webdriver.Chrome(options=options)
     driver.maximize_window()
 
-    yield driver 
+    yield driver
     driver.quit()
