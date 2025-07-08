3b10fbc3d254a8ee577b6597d075be53181a98ae
ShahabasAman <shezilaman@gmail.com>
2025-07-08 14:01:29 +0530

    Fix CI failures: browser headless mode

--- src/pageObjects/sidebar_menu_page.py
+++ src/pageObjects/sidebar_menu_page.py
@@ -2,7 +2,6 @@ from selenium.webdriver.common.by import By
 from src.base.base_page import BasePage
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-import time
 
 
 class SidebarMenuPage(BasePage):
@@ -16,36 +15,49 @@ class SidebarMenuPage(BasePage):
     def __init__(self, driver):
         super().__init__(driver)
 
-    # def open_menu(self):
-    #     self.click(self.MENU_BUTTON)
-
     def open_menu(self):
-    # Instead of storing element early, always find it fresh
+        """Open the sidebar menu with proper waits"""
+        self.wait_for_page_load()
         WebDriverWait(self.driver, self.timeout).until(
             EC.element_to_be_clickable(self.MENU_BUTTON)
         ).click()
-        time.sleep(0.5)
-
+        # Wait for sidebar animation to complete
+        WebDriverWait(self.driver, self.timeout).until(
+            EC.visibility_of_element_located(self.SIDEBAR)
+        )
 
     def go_to_history(self):
-        self.wait_for_page_load() 
-        self.open_menu()
-        self.wait_for_element(self.HISTORY_LINK)
-        self.click(self.HISTORY_LINK)
-
-    def go_to_home(self):
-        self.open_menu()
-        self.click(self.HOME_LINK)
-
-    def go_to_profile(self):
-        self.open_menu()
-        self.click(self.PROFILE_LINK)
-
-    def logout(self):
+        """Navigate to History page with proper waits"""
         self.wait_for_page_load()
         self.open_menu()
-        self.wait_for_element(self.LOGOUT_LINK)
-        self.click(self.LOGOUT_LINK)
+        WebDriverWait(self.driver, self.timeout).until(
+            EC.element_to_be_clickable(self.HISTORY_LINK)
+        ).click()
+        self.wait_for_page_load()
 
+    def go_to_home(self):
+        """Navigate to Home page with proper waits"""
+        self.wait_for_page_load()
+        self.open_menu()
+        WebDriverWait(self.driver, self.timeout).until(
+            EC.element_to_be_clickable(self.HOME_LINK)
+        ).click()
+        self.wait_for_page_load()
 
+    def go_to_profile(self):
+        """Navigate to Profile page with proper waits"""
+        self.wait_for_page_load()
+        self.open_menu()
+        WebDriverWait(self.driver, self.timeout).until(
+            EC.element_to_be_clickable(self.PROFILE_LINK)
+        ).click()
+        self.wait_for_page_load()
 
+    def logout(self):
+        """Logout with proper waits"""
+        self.wait_for_page_load()
+        self.open_menu()
+        WebDriverWait(self.driver, self.timeout).until(
+            EC.element_to_be_clickable(self.LOGOUT_LINK)
+        ).click()
+        self.wait_for_page_load()
